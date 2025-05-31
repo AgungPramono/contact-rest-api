a2c1c8e25593dd440186668aec56e2374c536764
agung <agungpermadi9@gmail.com>
2025-05-31 10:02:52 +0700

    enable cors

--- src/main/java/com/agung/restful/WebConfiguration.java
+++ src/main/java/com/agung/restful/WebConfiguration.java
@@ -2,14 +2,19 @@ package com.agung.restful;
 
 import com.agung.restful.resolver.UserArgumentResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+
+    @Value("${app.cors.allowed-origin}")
+    private String allowedOrigin;
 
     @Autowired
     private UserArgumentResolver userArgumentResolver;
@@ -18,5 +23,14 @@ public class WebConfiguration implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         WebMvcConfigurer.super.addArgumentResolvers(resolvers);
         resolvers.add(userArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigin)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
