a47d36a1af3d3fa871894dbd6e4a42dc7bdc39d9
agung <agungpermadi9@gmail.com>
2025-08-10 09:11:22 +0700

    revise UserControllerTest: fix TypeReference formatting

--- src/main/java/com/agung/restful/Application.java
+++ src/main/java/com/agung/restful/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BelajarSpringResTfulApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BelajarSpringResTfulApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
